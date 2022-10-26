6f985c85a01d5a32db25c776a747bf391a18deb6
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-26 10:05:14 +0600

    another section done

--- src/componentes/NextTrip/NextTrip.js
+++ src/componentes/NextTrip/NextTrip.js
@@ -9,6 +9,7 @@ import logo2 from "../../images/map icon.png";
 import logo3 from "../../images/send.png";
 import logo4 from "../../images/building 1.png";
 import logo5 from "../../images/heart (6) 1.png";
+import logo6 from "../../images/Ellipse 7.png";
 
 const NextTrip = () => {
   return (
@@ -77,6 +78,19 @@ const NextTrip = () => {
               <div>
                 <img src={logo5} alt="" />
               </div>
+            </div>
+          </div>
+          <div id="ongoin">
+            <div className="col-4">
+              <img src={logo6} alt="" />
+            </div>
+            <div className="col-6 text-contant">
+              <h6>Ongoin</h6>
+              <h4>Trip to </h4>
+              <p>40%</p>
+              <h5>
+                <span id="spann"></span>
+              </h5>
             </div>
           </div>
         </div>
